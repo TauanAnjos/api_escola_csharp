b178cd3f48005095939b8ca6f538b410f1468014
Tauan Dos Anjos Nascimento <tauan.nascimento@PGE.GOV-SE>
2025-06-23 11:49:10 -0300

    feat: crud aluno

--- api-escola/Controllers/AlunoController.cs
+++ api-escola/Controllers/AlunoController.cs
@@ -20,5 +20,39 @@ namespace api_escola.Controllers
             _service.CriarAluno(request);
             return Created("api/v1/aluno", request);
         }
+        [HttpGet("{id}")]
+        public IActionResult BuscarAlunoPorId([FromRoute]ulong id)
+        {
+            var alunoExistente = _service.BuscarAluno(id);
+
+            if (alunoExistente == null)
+            {
+                return NotFound("Cliente de ID: " + id + " não encontrado.");
+            }
+            return Ok(alunoExistente);
+        }
+        [HttpPut("{id}")]
+        public IActionResult EditarAluno([FromRoute]ulong id, [FromBody]AlunoDtoRequest request)
+        {
+            var alunoExistente = _service.BuscarAluno(id);
+            if(alunoExistente == null)
+            {
+                return NotFound("Cliente de ID: " + id + " não encontrado.");
+            }
+            _service.EditarAluno(id, request);
+            var alunoAtualizado = _service.BuscarAluno(id);
+            return Ok(alunoAtualizado);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeletarAluno([FromRoute] ulong id)
+        {
+            var alunoExistente = _service.BuscarAluno(id);
+            if (alunoExistente == null)
+            {
+                return NotFound("Cliente de ID: " + id + " não encontrado.");
+            }
+            _service.DeletarAluno(id);
+            return Ok("Aluno deletado com sucesso.");
+        }
     }
 }
